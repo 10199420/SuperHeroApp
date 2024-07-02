c958f8f2e0597cd9dfe03773c8cf03042603b9ed
brayyanchristophertrujillovalle <brayyantrujil@gmail.com>
2024-07-02 17:12:47 -0500

    Initial Commit

---
+++ SuperHeroApp/SuperHeroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroAppApp.swift
+//  SuperHeroApp
+//
+//  Created by Brayyan Christopher Trujillo Valle on 2/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
